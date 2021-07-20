1d71efc34759836f7c86c68c9d043ccc66273994
Redwan G <redwan@macademia.fr>
2021-07-20 10:53:48 +0200

    ajout des fichiers css, bootstrap et header

---
+++ header.php
@@ -0,0 +1,9 @@
+<!DOCTYPE >
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <title>PPE</title>
+    <link rel="stylesheet" href="./styles.css" media="all">
+    <link rel="stylesheet" type="text/css" href="./bootstrap.css">
+</head>
