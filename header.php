55bad5a84efc50e9457cb939866bbfd6c35cf4f9
Redwan G <redwan@macademia.fr>
2021-07-31 13:29:53 +0200

    rien de bien nouveau

--- header.php
+++ header.php
@@ -3,7 +3,12 @@
 
 <head>
     <meta charset="UTF-8">
+
     <title>PPE</title>
+
     <link rel="stylesheet" href="./styles.css" media="all">
+
+    <!-- Bootstrap core CSS -->
     <link rel="stylesheet" type="text/css" href="./bootstrap.css">
+
 </head>
--- index.php
+++ index.php
@@ -5,6 +5,7 @@ include "header.php";
 
 
 
+
 echo("hello world")
 
 
